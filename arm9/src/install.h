9e142264b74902e0465b35ce08fe449697e13278
FlameKat53 <flame9804@yahoo.com>
2019-11-08 11:53:38 -0600

    part 1

--- arm9/src/install.h
+++ arm9/src/install.h
@@ -3,6 +3,6 @@
 
 #include <nds/ndstypes.h>
 
-bool install(char* fpath, bool Install04, bool Install05, bool Install15, bool Install17);
+bool install(char* fpath, bool Install00, bool Install01, bool Install04, bool Install05, bool Install11, bool Install15, bool Install17);
 
 #endif
